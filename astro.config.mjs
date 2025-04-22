58a0e14999b8c3566a7febd2e444ae3f722bb389
tsots <mosalatsotetsi@gmail.com>
2025-04-22 12:35:33 +0200

    changed prefix index page again

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tsots.github.io',
-  base: '/mona'
+  base: import.meta.env.VITE_BASE_PATH || '/mona',
 });
